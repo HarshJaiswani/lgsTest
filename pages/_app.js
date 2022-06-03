40d4f7b7a526ca02d78be7c4d9dc098163fc4769
Harsh Jaiswani <harshjaiswani2003@gmail.com>
2022-06-03 23:43:15 +0530

    apis created

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import react , { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loggedIn , setLoggedIn] = useState(false);
+  return (
+    <>
+    <Head>
+        <meta
+          name="description"
+          content="LeGroSh is a App to provide web services , self growth blogs , Tech related content and a Engine to communicate with all! , solve queries and doubts to provide a proper guidence to my juniors - An initiative by Harsh Jaiswani"
+        />
+        <noscript>
+          Give Power of Javascript to Your Browser Engine to Have Embrase the
+          Beauty of this App
+        </noscript>
+        <title>LeGroSh - Learn-Grow-Share</title>
+      </Head>
+  <Component {...pageProps} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+  </>
+  );
 }
 
 export default MyApp
